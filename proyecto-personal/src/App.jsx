fa0a4abfaed005216ecf603fdd5d31ab8d1c54e5
Aleho16 <perez.alejo.rojas@gmail.com>
2026-06-24 13:52:06 -0500

    agrego buscador

--- proyecto-personal/src/App.jsx
+++ proyecto-personal/src/App.jsx
@@ -4,8 +4,13 @@ function App() {
   return (
     <div>
       <h1>Buscador de pelis </h1>
+      <form>
+        <input type="text"
+        placeholder='escribe el nombre de tu pelicula' />
+        <button type='submit'>Buscar</button>
+      </form>
     </div>
-    
+
   )
 }
 
