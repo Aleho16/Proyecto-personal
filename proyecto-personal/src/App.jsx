e8c6b206c59cc5507f4084f34e295ce95a01df7e
Aleho16 <perez.alejo.rojas@gmail.com>
2026-06-24 14:01:38 -0500

    se añade la memoria con usestate al buscador

--- proyecto-personal/src/App.jsx
+++ proyecto-personal/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [busqueda, setbusqueda]= useState('')
+  const hacerbusqueda = (e) => {
+      e.preventDefault()
+      console.log("el ususario quiere buscar ",busqueda)
+  }
   return (
     <div>
       <h1>Buscador de pelis </h1>
       <form>
         <input type="text"
-        placeholder='escribe el nombre de tu pelicula' />
+        placeholder='escribe el nombre de tu pelicula'
+        value={busqueda}
+        onChange={(e) => setbusqueda(e.target.value)} />
         <button type='submit'>Buscar</button>
       </form>
     </div>
